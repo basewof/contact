3064a1704ad8688139c9250ad76a4929bf8ca482
basewof <basewof@gmail.com>
2025-10-22 18:14:17 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <div>Hi, I would like to move the conversation about the flash USDT completely away from Upwork if you dont mind. You can contact me through any of the following</div>
+      <div>Hi, I would like to move the conversation away from Upwork if you dont mind. You can contact me through any of the following</div>
       <div>
         <p>Name: Dipe Kehinde</p>
         <p>Email: dipekehinde@gmail.com</p>
