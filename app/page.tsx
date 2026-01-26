0da9b3fd21b2f0a1d89bc7638e725c22c7850c8d
basewof <basewof@gmail.com>
2026-01-26 16:17:38 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,12 @@ export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <div>Hi, I would like to move the conversation away from Upwork if you dont mind. You can contact me through any of the following</div>
+      <div>Also if you don't mind, I will prefer to receive any remaining payment off-upwork or as crypto</div>
       <div>
         <p>Name: Dipe Kehinde</p>
         <p>Email: dipekehinde@gmail.com</p>
         <p>WhatsApp: +2348167774084</p>
-        <p>Telegram: @p_hronesis</p>
+        <p>Telegram: @Hex_Nicklas</p>
       </div>
     </div>
   );
